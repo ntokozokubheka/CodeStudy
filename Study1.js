068f27c03048c42be0b3dc706666ab9c79bb639c
Stokza <37531253+ntokozokubheka@users.noreply.github.com>
2023-09-15 13:53:36 +0200

    Update Study1.js

--- Study1.js
+++ Study1.js
@@ -43,4 +43,21 @@ function smallestCommons(arr) {
   }
 }
 
+
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  // Using a regex, replace characters with it's corresponding html entity
+  return str.replace(/([&<>\"'])/g, match => htmlEntities[match]);
+}
+
+// test here
+convertHTML("Dolce & Gabbana");
+
 smallestCommons([1, 5]);
