b06aa9c6e7f8939729c270eebdd89a08df84d6e8
Stokza <37531253+ntokozokubheka@users.noreply.github.com>
2023-09-15 11:26:11 +0200

    Update Study1.js

--- Study1.js
+++ Study1.js
@@ -22,3 +22,25 @@ function sumPrimes(num) {
   }
   return primes.reduce((sum, prime) => sum + prime, 0);
 }
+
+
+//The smallest common multiple between two numbers is the smallest number that both numbers can divide into evenly. This concept can be extended to more than two numbers as well.
+function smallestCommons(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const range = Array(max - min + 1)
+    .fill(0)
+    .map((_, i) => i + min);
+  // Largest possible value for SCM
+  const upperBound = range.reduce((prod, curr) => prod * curr);
+  // Test all multiples of 'max'
+  for (let multiple = max; multiple <= upperBound; multiple += max) {
+    // Check if every value in range divides 'multiple'
+    const divisible = range.every((value) => multiple % value === 0);
+    if (divisible) {
+      return multiple;
+    }
+  }
+}
+
+smallestCommons([1, 5]);
